8dbcd9539c134c5b8a04c3387f1f8667d0990537
Aleksandr <bas8319@yahoo.com>
2022-09-11 17:09:17 +0300

    Added information about function to the file lecture #1

--- Lectures/Lecture1_bases.py
+++ Lectures/Lecture1_bases.py
@@ -193,3 +193,33 @@ iter **= 5  # iter = iter ** 5  ЭТО ВОЗВЕДЕНИЕ В СТЕПЕНЬ
 # colors.append('gray')                   # добавить в конец
 # print(colors == ['red', 'green', 'blue', 'gray'])  # True
 # colors.remove('red')                    # del colors[0] # удалить элемент
+
+
+################################################ Функции                                          ########
+################################################ Это фрагмент программы, используемый многократно ########
+
+# def function_name(x):
+# body line 1
+# . . .
+# body line n
+# optional return
+
+# def f(x):
+#     return x**2
+
+
+# def f(x):
+#     if x == 1:
+#         return 'Целое'
+#     elif x == 2.3:
+#         return 23
+#     else:
+#         return
+
+
+# print(f(1))                           # Целое
+# print(f(2.3))                         # 23
+# print(f(28))                          # None
+# print(type(f(1)))                     # str
+# print(type(f(2.3)))                   # int
+# print(type(f(28)))                    # NoneType
