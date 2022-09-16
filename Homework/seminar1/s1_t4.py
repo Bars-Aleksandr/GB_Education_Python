245490d251f0d31705c214aa1d448baa12810526
Aleksandr <bas8319@yahoo.com>
2022-09-16 21:40:50 +0300

    solution of task No. 4 seminar No. 1 successfully

--- Homework/seminar1/s1_t4.py
+++ Homework/seminar1/s1_t4.py
@@ -4,11 +4,11 @@
 num_quar = int(input('Введите номер четверти: '))
 if num_quar < 1 or num_quar > 4:
     print('Четверти с таким номером не существует')
-elif n == 1:
-    print('x > 0 and y > 0')
-elif n == 2:
-    print('x < 0 and y > 0')
-elif n == 3:
-    print('x < 0 and y < 0')
-elif n == 4:
-    print('x > 0 and y < 0')
+elif num_quar == 1:
+    print('x > 0, y > 0')
+elif num_quar == 2:
+    print('x < 0, y > 0')
+elif num_quar == 3:
+    print('x < 0, y < 0')
+elif num_quar == 4:
+    print('x > 0, y < 0')
