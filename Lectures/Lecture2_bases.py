cd9ad813c152f89c82d28264f4cd3094fd291f01
Aleksandr <bas8319@yahoo.com>
2022-09-18 16:30:43 +0300

    Added : How to work with tuples

--- Lectures/Lecture2_bases.py
+++ Lectures/Lecture2_bases.py
@@ -143,3 +143,24 @@
 # for e in range(1, 10):
 #  list.append(fib(e))
 # print(list)  # 1 1 2 3 5 8 13 21 34
+
+
+############################################ Кортежи - неизменяемый список #################
+# t = ()
+# print(type(t))  # tuple
+# t = (1,)
+# print(type(t))  # tuple
+# t = (1)
+# print(type(t))  # int
+# t = (28, 9, 1990)
+# print(type(t))  # tuple
+# colors = ['red', 'green', 'blue']
+# print(colors)  # ['red', 'green', 'blue']
+# t = tuple(colors)
+# print(t)  # ('red', 'green', 'blue')
+
+
+# # t = tuple(['red', 'green', 'blue'])
+# red, green, blue = t
+# print('r:{} g:{} b:{}'.format(red, green, blue))
+# # r:red g:green b:blue
