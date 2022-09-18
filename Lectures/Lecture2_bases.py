38ef7ac40c94b109bc69279a8a759517fdbc9033
Aleksandr <bas8319@yahoo.com>
2022-09-18 16:22:14 +0300

    Added : How to work with recursion

--- Lectures/Lecture2_bases.py
+++ Lectures/Lecture2_bases.py
@@ -129,3 +129,17 @@
 # print(concatenatio('a', 's', 'd', 'w'))  # asdw
 # print(concatenatio('a', '1', 'd', '2'))  # a1d2
 # # print(conatenatio(1, 2, 3, 4)) # TypeError: ...
+
+
+# №№№№№№№№№№№№№№№№№№№№№№№№№№№№№№ Рекурсия  №№№№№№№№№№№№№№№№№№
+# def fib(n):
+#  if n in [1, 2]:
+#  return 1             НЕЗАБЫВАЕМ ОБ УСЛОВИИ ПРИ КОТОРОМ НАДО ВЫЙТИ ИЗ ФУНКЦИИ
+#  else:
+#  return fib(n-1) + fib(n-2)
+
+
+# list = []
+# for e in range(1, 10):
+#  list.append(fib(e))
+# print(list)  # 1 1 2 3 5 8 13 21 34
