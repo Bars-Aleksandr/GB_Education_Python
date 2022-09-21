348633cd3f1470dfde4b65a2774b8018a7b51271
Aleksandr <bas8319@yahoo.com>
2022-09-21 11:17:42 +0300

    added a solution to the fourth task

--- Homework/seminar2/s2_t1.py
+++ Homework/seminar2/s2_t1.py
@@ -29,12 +29,47 @@
 
 # Задайте список из n чисел последовательности $(1 +\frac 1 n) ^ n$ и выведите на экран их сумму.
 
-numbers = int(input('Введите число n: '))
-list_numbers = []
-sum_elements = 0
-for n in range(1, numbers+1):
-    a = round((1 + 1 / n) ** n, 2)
-    sum_elements = sum_elements + a
-    list_numbers.insert(n, a)
-print(list_numbers)
-print('Сумма элементов полученной последовательности', sum_elements)
+# numbers = int(input('Введите число n: '))
+# list_numbers = []
+# sum_elements = 0
+# for n in range(1, numbers+1):
+#     a = round((1 + 1 / n) ** n, 2)
+#     sum_elements = sum_elements + a
+#     list_numbers.insert(n, a)
+# print(list_numbers)
+# print('Сумма элементов полученной последовательности', sum_elements)
+
+# 33
+
+# Задайте список из N элементов, заполненных числами из промежутка[-N, N].
+# Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
+
+from random import randint
+
+
+def create_rnd_list(n):
+    rnd_list = [randint(-n, n) for i in range(n)]
+    return rnd_list
+
+
+def create_file_position(n):
+    amount = randint(1, n)
+    data = open('file.txt', 'w')
+    for i in range(amount):
+        a = randint(1, n)
+        data.write(str(a)+"\n")
+
+
+number = int(input('Введите число N: '))
+list = create_rnd_list(number)
+create_file_position(number)
+
+p = 1
+with open(r"file.txt", "r") as position_file:
+    print('Позиции из файла file.txt')
+    for i in position_file:
+        print(i, end='')
+        pp = int(i)
+        p *= list[pp-1]
+print('Сгенерированый список', list)
+print('Произведения чисел ', p)
