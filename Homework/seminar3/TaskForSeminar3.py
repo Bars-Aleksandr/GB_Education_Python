5e3b43b07cd09df16c79c25963edf614609d43e1
Aleksandr <bas8319@yahoo.com>
2022-09-26 16:04:39 +0300

    Added the solution of the fourth homework task  to the seminar3

--- Homework/seminar3/TaskForSeminar3.py
+++ Homework/seminar3/TaskForSeminar3.py
@@ -56,21 +56,40 @@
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
 
-import random
+# import random
 
 
-def create_rnd_list(n):
-    rnd_list = [round(random.uniform(1, n), 2) for i in range(n)]
-    return rnd_list
+# def create_rnd_list(n):
+#     rnd_list = [round(random.uniform(1, n), 2) for i in range(n)]
+#     return rnd_list
 
 
-def fraction(x): return x % 1
+# def fraction(x): return x % 1
 
 
-#n = int(input('Enter the number of list items: '))
-n = 5
-list = create_rnd_list(n)
-print('My list: ', list)
-list2 = sorted([round(fraction(list[i]), 2) for i in range(n)])
-print(list2)
-print(list, '=>', round(list2[n-1]-list2[0], 2))
+# #n = int(input('Enter the number of list items: '))
+# n = 5
+# list = create_rnd_list(n)
+# print('My list: ', list)
+# list2 = sorted([round(fraction(list[i]), 2) for i in range(n)])
+# print(list2)
+# print(list, '=>', round(list2[n-1]-list2[0], 2))
+##################################################################################################
+# Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+# Пример:
+
+# - 45 -> 101101
+# - 3 -> 11
+# - 2 -> 10
+def dec_in_bin(num):
+    bin = ''
+    while num != 0:
+        bin = bin + str(num % 2)
+        num = num // 2
+    return bin
+
+
+dec = int(input('Enter a Number in DEC format: '))
+bin = dec_in_bin(dec)
+print(dec, '->', bin[::-1])
