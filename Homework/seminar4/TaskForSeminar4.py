3df6633d635035d55e40971f4b7c4eb8cc141f0d
Aleksandr <bas8319@yahoo.com>
2022-10-05 19:53:52 +0300

    Task#3 for the reminar4 - after watching seminar

--- Homework/seminar4/TaskForSeminar4.py
+++ Homework/seminar4/TaskForSeminar4.py
@@ -54,10 +54,72 @@
 def unique_list(entered_list):
     list = []
     for i in entered_list:
-        if i not in list:
+        if entered_list.count(i) == 1:
             list.append(i)
     return list
 
 
 entered_list = list(map(int, input('Enter list: ').split()))
 print(unique_list(entered_list))
+########################################################################################################
+# Задана натуральная степень k.
+# Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена
+# и записать в файл многочлен степени k.
+
+# Пример:
+
+# - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+# from random import randint
+
+
+# def get_elemements(k):
+#     elements = [randint(0, 10) for i in range(k+1)]
+#     return (elements)
+
+
+# def get_polinom(k, elements):
+#     elements_str = str(elements)
+#     print(type(elements_str[0]))
+#     # polinom = []
+#     # var_x = []
+#     for i in elements_str:
+#         #     var_x.append('x^')
+#         print(i, end='\n')
+#     #     print(var_x)
+
+
+# degree = int(input('Enter natural degree from 1 to 100: '))
+# basic_elements = get_elemements(degree)
+# get_polinom(degree, basic_elements)
+# polynomial = ['2*x ^ 9', ' +', '10*x ^ 8', ' +', '8*x ^ 7', ' +', '4*x ^ 6', ' +',
+#               '9*x ^ 5', ' +', '10*x ^ 4', ' +', '2*x ^ 3', ' +', '2*x2', ' +', '10', '= 0']
+# # print('basic list', basic_elements)
+# print("".join(map(str, polynomial)).replace(' 1*x', ' x'))
+# from random import randint
+
+# k = int(input('Insert equation power: '))
+# koefs = list()
+# for i in range(1, k + 2):
+#     koefs.append(randint(1, 100))
+
+# ans = list()
+# for i in range(len(koefs)):
+#     if k == 1:
+#         ans.append(f'{koefs[i]}*x')
+#     elif k == 0:
+#         ans.append(f'{koefs[i]}')
+#     else:
+#         ans.append(f'{koefs[i]}*x**{k}')
+#     flag = randint(0, 1)
+#     if flag == 1:
+#         ans.append('+')
+#     elif flag == 0:
+#         ans.append('-')
+#     k -= 1
+
+# ans.pop(-1)
+# ans.append('=0')
+# print(''.join(ans))
+# fout = open('output.txt', 'w')
+# fout.write(''.join(ans))
+# fout.close()
