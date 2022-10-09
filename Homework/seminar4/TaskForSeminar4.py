c1440c0f3d02ca1b9bc29df2f3b04b16ec8307a9
Aleksandr <bas8319@yahoo.com>
2022-10-09 11:03:23 +0300

    Task#4 for the reminar4

--- Homework/seminar4/TaskForSeminar4.py
+++ Homework/seminar4/TaskForSeminar4.py
@@ -51,16 +51,19 @@
 # Задайте последовательность чисел.
 # Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 
-def unique_list(entered_list):
-    list = []
-    for i in entered_list:
-        if entered_list.count(i) == 1:
-            list.append(i)
-    return list
+# from random import randint
 
 
-entered_list = list(map(int, input('Enter list: ').split()))
-print(unique_list(entered_list))
+# def unique_list(entered_list):
+#     list = []
+#     for i in entered_list:
+#         if entered_list.count(i) == 1:
+#             list.append(i)
+#     return list
+
+
+# entered_list = list(map(int, input('Enter list: ').split()))
+# print(unique_list(entered_list))
 ########################################################################################################
 # Задана натуральная степень k.
 # Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена
@@ -69,57 +72,39 @@ print(unique_list(entered_list))
 # Пример:
 
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
-# from random import randint
 
 
-# def get_elemements(k):
-#     elements = [randint(0, 10) for i in range(k+1)]
-#     return (elements)
+from random import randint
 
 
-# def get_polinom(k, elements):
-#     elements_str = str(elements)
-#     print(type(elements_str[0]))
-#     # polinom = []
-#     # var_x = []
-#     for i in elements_str:
-#         #     var_x.append('x^')
-#         print(i, end='\n')
-#     #     print(var_x)
+def get_сoefficient(k):
+    сoefficient = [randint(0, 10) for i in range(k+1)]
+    return (сoefficient)
 
 
-# degree = int(input('Enter natural degree from 1 to 100: '))
-# basic_elements = get_elemements(degree)
-# get_polinom(degree, basic_elements)
-# polynomial = ['2*x ^ 9', ' +', '10*x ^ 8', ' +', '8*x ^ 7', ' +', '4*x ^ 6', ' +',
-#               '9*x ^ 5', ' +', '10*x ^ 4', ' +', '2*x ^ 3', ' +', '2*x2', ' +', '10', '= 0']
-# # print('basic list', basic_elements)
-# print("".join(map(str, polynomial)).replace(' 1*x', ' x'))
-# from random import randint
+def get_polinom(k, elements):
+    var_x = []
+    for i in elements:
+        if k > 1:
+            var_x.append(f'{i}x^{k}')
+        elif k == 1:
+            var_x.append(f'{i}x')
+        elif k == 0:
+            var_x.append(f'{i}')
+        flag = randint(0, 1)
+        if flag == 1:
+            var_x.append('+')
+        elif flag == 0:
+            var_x.append('-')
+        k -= 1
+    var_x[-1] = '=0'
+    return var_x
 
-# k = int(input('Insert equation power: '))
-# koefs = list()
-# for i in range(1, k + 2):
-#     koefs.append(randint(1, 100))
 
-# ans = list()
-# for i in range(len(koefs)):
-#     if k == 1:
-#         ans.append(f'{koefs[i]}*x')
-#     elif k == 0:
-#         ans.append(f'{koefs[i]}')
-#     else:
-#         ans.append(f'{koefs[i]}*x**{k}')
-#     flag = randint(0, 1)
-#     if flag == 1:
-#         ans.append('+')
-#     elif flag == 0:
-#         ans.append('-')
-#     k -= 1
-
-# ans.pop(-1)
-# ans.append('=0')
-# print(''.join(ans))
-# fout = open('output.txt', 'w')
-# fout.write(''.join(ans))
-# fout.close()
+degree = int(input('Enter natural number from 1 to 100: '))
+basic_elements = get_сoefficient(degree)
+polinom = get_polinom(degree, basic_elements)
+print(''.join(polinom))
+out_file = open('file.txt', 'w')
+out_file.write(''.join(polinom))
+out_file.close()
